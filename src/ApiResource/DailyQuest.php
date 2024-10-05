50fb5e29051702f284e1d77466603c2dc855fcf8
Darek Dawidowicz <ddaw@wp.pl>
2024-10-05 11:54:14 +0200

    Relating Custom ApiResources

--- src/ApiResource/DailyQuest.php
+++ src/ApiResource/DailyQuest.php
@@ -7,6 +7,7 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
+use App\Entity\DragonTreasure;
 use App\Enum\DailyQuestStatusEnum;
 use App\State\DailyQuestStateProcessor;
 use App\State\DailyQuestStateProvider;
@@ -26,11 +27,16 @@ class DailyQuest
 {
     #[Ignore]
     public \DateTimeInterface $day;
+
     public string $questName;
     public string $description;
     public int $difficultyLevel;
     public DailyQuestStatusEnum $status;
     public \DateTimeInterface $lastUpdated;
+    /**
+     * @var DragonTreasure[]
+     */
+    public array $treasures = [];
 
     public function __construct(\DateTimeInterface $day)
     {
