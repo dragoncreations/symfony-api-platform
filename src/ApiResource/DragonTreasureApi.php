7687fe61b4d9bd4c45e2e0be7a46b4c09b6d04ca
Darek Dawidowicz <ddaw@wp.pl>
2024-10-08 10:29:03 +0200

    Quick! Create a DragonTreasure DTO

---
+++ src/ApiResource/DragonTreasureApi.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\ApiResource;
+
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use App\Entity\DragonTreasure;
+use App\State\EntityClassDtoStateProcessor;
+use App\State\EntityToDtoStateProvider;
+
+#[ApiResource(
+    shortName: 'Treasure',
+    paginationItemsPerPage: 10,
+    provider: EntityToDtoStateProvider::class,
+    processor: EntityClassDtoStateProcessor::class,
+    stateOptions: new Options(entityClass: DragonTreasure::class),
+)]
+class DragonTreasureApi
+{
+    #[ApiProperty(readable: false, writable: false, identifier: true)]
+    public ?int $id = null;
+
+    public ?string $name = null;
+
+    public ?string $description = null;
+
+    public int $value = 0;
+
+    public int $coolFactor = 0;
+
+    public ?UserApi $owner = null;
+
+    public ?string $shortDescription = null;
+
+    public ?string $plunderedAtAgo = null;
+
+    public ?bool $isMine = null;
+}
